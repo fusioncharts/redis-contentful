eb53768e35db3a2f9468149b7244d56aecacd6d4
shreyas-a <shreyas.a24@gmail.com>
2018-03-18 01:31:03 +0530

    Begin 🚀

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# cf-redis
+# Contentful Redis
+
+A tiny library to map Contentful ☁️ space into Redis ⚡️
+
+## Installation
+
+```sh
+npm i -S cf-redis
+```
+
+## Usage
+
+```js
+import cfRedis from "cf-redis";
+
+const client = new cfRedis({
+  space: "<Space ID>",
+  accessToken: "<Access Token>"
+});
+
+(async () => {
+  // Gets the latest space content from Contentful and dumps it in your Redis server 🎉
+  await client.sync();
+
+  // Gets all data directly from Redis 🚀
+  const response = await client.get();
+  console.log(response);
+})();
+```
+
+
+```js
+// You'll get an object with your content type ID's as keys and their values as array of content objects. The keys (content types) will be prefixed with cf-redis for uniquely identify keys created by cf-redis 🤓
+{
+    "cf-redis:<Your ContentType ID>": [{}, {}, {}]
+}
+```
+
+License
+----
+
+MIT
---
+++ index.js
@@ -0,0 +1,87 @@
+const { promisify } = require("util");
+const redis = require("redis");
+const contentful = require("contentful");
+
+class CFRedis {
+  constructor(details) {
+    this.nextSyncToken = "";
+
+    this.cfClient = contentful.createClient({
+      space: details.space,
+      accessToken: details.accessToken
+    });
+
+    this.redisClient = redis.createClient();
+  }
+
+  // Public Methods
+  async sync() {
+    const isInitial = this.nextSyncToken === "";
+
+    const response = await this.cfClient.sync({
+      ...(isInitial && { initial: true }),
+      ...(!isInitial && { nextSyncToken: this.nextSyncToken })
+    });
+
+    this.nextSyncToken = response.nextSyncToken;
+
+    if (response.entries && response.entries.length) {
+      const promises = [];
+      response.entries.forEach(async entry => {
+        const metadata = entry.sys;
+        const contentType = metadata.contentType.sys.id;
+
+        switch (metadata.type) {
+          case "Entry":
+            const hset = promisify(this.redisClient.hset).bind(
+              this.redisClient
+            );
+            promises.push(
+              hset(
+                `cf-redis:${contentType}`,
+                metadata.id,
+                JSON.stringify(entry.fields)
+              )
+            );
+            break;
+          case "Delete":
+            const hdel = promisify(this.redisClient.hdel).bind(
+              this.redisClient
+            );
+            promises.push(hdel(`cf-redis:${contentType}`, metadata.id));
+            break;
+          default:
+            break;
+        }
+      });
+
+      return Promise.all(promises);
+    }
+  }
+
+  async get(contentType, id) {
+    const scan = promisify(this.redisClient.scan).bind(this.redisClient);
+    const hgetall = promisify(this.redisClient.hgetall).bind(this.redisClient);
+    const response = await scan(
+      "0",
+      "MATCH",
+      `cf-redis:${contentType ? contentType : "*"}`
+    );
+    const hashes = response[1] || [];
+
+    const promises = hashes.map(hash => hgetall(hash));
+    const responses = await Promise.all(promises);
+
+    return hashes.reduce(
+      (final, value, index) =>
+        Object.assign(final, {
+          [value]: Object.keys(responses[index]).map(
+            key => responses[index][key]
+          )
+        }),
+      {}
+    );
+  }
+}
+
+module.exports = CFRedis;
