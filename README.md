89eee9c3f05d64a8a1fddc573e4de5969e5bcf50
shreyas-a <shreyas.a24@gmail.com>
2018-03-28 14:12:07 +0530

    🐞 fixed deleting unpublished keys from redis

--- README.md
+++ README.md
@@ -1,12 +1,6 @@
-<div align="center">
-
-<h1>Redis Contentful</h1>
-
-[![npm](https://img.shields.io/npm/v/redis-contentful.svg)](https://www.npmjs.com/package/redis-contentful) [![license](https://img.shields.io/github/license/shreyas-a/redis-contentful.svg)](https://github.com/shreyas-a/redis-contentful/blob/master/LICENSE)
+# Redis Contentful [![npm](https://img.shields.io/npm/v/redis-contentful.svg)](https://www.npmjs.com/package/redis-contentful) [![license](https://img.shields.io/github/license/shreyas-a/redis-contentful.svg)](https://github.com/shreyas-a/redis-contentful/blob/master/LICENSE)
 
 A tiny library to map Contentful ☁️ space into Redis ⚡️
-
-</div>
 
 ## Why should I care?
 
@@ -82,7 +76,7 @@ You'll get an object with your content type ID's as keys and their values as arr
 
 ## Redis Store
 
-> In Redis, the keys (content types) will be prefixed with `redis-contentful` to uniquely identify keys created by `redis-contentful` 🤓
+> In Redis, the keys (entries) will be prefixed with their respective content type 🤓
 
 ## License
 
--- src/index.js
+++ src/index.js
@@ -32,87 +32,105 @@ const extract = (data, locale) => {
 
 class RedisContentful {
   constructor({ redis, contentful }) {
-    this.redisClient = Redis.createClient();
-    this.redisClient.select((redis && redis.database) || 0);
+    try {
+      this.redisClient = Redis.createClient();
+      this.redisClient.select((redis && redis.database) || 0);
 
-    this.cfClient = Contentful.createClient({
-      space: contentful.space,
-      accessToken: contentful.accessToken,
-    });
-    this.nextSyncToken = '';
-    this.locale = contentful.locale || 'en-US';
+      this.cfClient = Contentful.createClient({
+        space: contentful.space,
+        accessToken: contentful.accessToken,
+      });
+      this.locale = contentful.locale || 'en-US';
+    } catch (error) {
+      return error;
+    }
   }
 
   // Public Methods
   async sync() {
-    const isInitial = this.nextSyncToken === '';
+    try {
+      const set = promisify(this.redisClient.set).bind(this.redisClient);
+      const scan = promisify(this.redisClient.scan).bind(this.redisClient);
+      const hget = promisify(this.redisClient.hget).bind(this.redisClient);
+      const hset = promisify(this.redisClient.hset).bind(this.redisClient);
+      const del = promisify(this.redisClient.del).bind(this.redisClient);
 
-    const response = await this.cfClient.sync({
-      ...(isInitial && { initial: true }),
-      ...(!isInitial && { nextSyncToken: this.nextSyncToken }),
-    });
-
-    this.nextSyncToken = response.nextSyncToken;
-
-    if (response.entries && response.entries.length) {
       const promises = [];
-      response.entries.forEach(async entry => {
-        const metadata = entry.sys;
-        const contentType = metadata.contentType.sys.id;
+      const nextSyncToken = await hget('redis-contentful', 'nextSyncToken');
+      const isInitial = !nextSyncToken;
 
-        switch (metadata.type) {
-          case 'Entry': {
-            const hset = promisify(this.redisClient.hset).bind(
-              this.redisClient
-            );
-            promises.push(
-              hset(
-                `redis-contentful:${contentType}`,
-                metadata.id,
-                JSON.stringify(entry)
-              )
-            );
-            break;
-          }
-          case 'Delete': {
-            const hdel = promisify(this.redisClient.hdel).bind(
-              this.redisClient
-            );
-            promises.push(hdel(`redis-contentful:${contentType}`, metadata.id));
-            break;
-          }
-          default:
-            break;
-        }
+      if (isInitial) {
+        const flushall = promisify(this.redisClient.flushall).bind(
+          this.redisClient
+        );
+        await flushall();
+      }
+
+      const response = await this.cfClient.sync({
+        ...(isInitial && { initial: true }),
+        ...(!isInitial && { nextSyncToken }),
       });
 
+      // Adding all new entries in redis
+      if (response.entries && response.entries.length) {
+        response.entries.forEach(async entry => {
+          const { sys } = entry;
+          const contentType = sys.contentType.sys.id;
+          promises.push(set(`${contentType}:${sys.id}`, JSON.stringify(entry)));
+        });
+      }
+
+      // Deleting all the deleted entries from redis
+      if (response.deletedEntries && response.deletedEntries.length) {
+        response.deletedEntries.forEach(async entry => {
+          const { sys } = entry;
+          const responseKey = await scan('0', 'MATCH', `*:${sys.id}`);
+
+          if (responseKey[1]) {
+            promises.push(del(responseKey[1]));
+          }
+        });
+      }
+
+      await hset('redis-contentful', 'nextSyncToken', response.nextSyncToken);
+
       await Promise.all(promises);
+      return Promise.resolve({ message: 'Sync Complete' });
+    } catch (error) {
+      return Promise.reject(new Error({ message: 'Sync Failed' }));
     }
-    return Promise.resolve({ message: 'Sync Complete' });
   }
 
   async get(contentType) {
-    const scan = promisify(this.redisClient.scan).bind(this.redisClient);
-    const hgetall = promisify(this.redisClient.hgetall).bind(this.redisClient);
-    const response = await scan(
-      '0',
-      'MATCH',
-      `redis-contentful:${contentType || '*'}`
-    );
-    const hashes = response[1] || [];
+    try {
+      const get = promisify(this.redisClient.get).bind(this.redisClient);
+      const scan = promisify(this.redisClient.scan).bind(this.redisClient);
 
-    const promises = hashes.map(hash => hgetall(hash));
-    const responses = await Promise.all(promises);
+      const response = await scan(
+        '0',
+        'MATCH',
+        `${contentType ? `${contentType}:*` : '*:*'}`
+      );
+      const keys = response[1] || [];
 
-    return hashes.reduce(
-      (final, value, index) =>
-        Object.assign(final, {
-          [value.split('redis-contentful:').pop()]: Object.keys(
-            responses[index]
-          ).map(key => extract(JSON.parse(responses[index][key]), this.locale)),
-        }),
-      {}
-    );
+      const promises = keys.map(key => get(key));
+      const responses = await Promise.all(promises);
+
+      return keys.reduce((final, value, index) => {
+        if (final[value.split(':').shift()]) {
+          final[value.split(':').shift()].push(
+            extract(JSON.parse(responses[index]), this.locale)
+          );
+        } else {
+          final[value.split(':').shift()] = [
+            extract(JSON.parse(responses[index]), this.locale),
+          ];
+        }
+        return final;
+      }, {});
+    } catch (error) {
+      return Promise.reject(new Error({ message: 'Get Failed' }));
+    }
   }
 }
 
